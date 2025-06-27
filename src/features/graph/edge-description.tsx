a3ece4f3c1cc8bd22e94e3296ddb86571733f96b
Miles Wells <mileswells93@gmail.com>
2025-06-27 17:44:07 -0400

    more details

--- src/features/graph/edge-description.tsx
+++ src/features/graph/edge-description.tsx
@@ -122,7 +122,7 @@ function ItemEdgeCost({ edge, label }: { edge: Extract<Edge, ItemEdge | ItemTaxi
 						{edge.item}
 					</Text>
 
-					<ItemContent item={edge.item} />
+					<ItemContent edge={edge} />
 
 					<ItemConsumption item={edge.item} />
 				</Center>
@@ -131,7 +131,7 @@ function ItemEdgeCost({ edge, label }: { edge: Extract<Edge, ItemEdge | ItemTaxi
 	);
 }
 
-function ItemContent({ item }: { item: Item }) {
+function ItemContent({ edge: { from, to, item } }: { edge: Extract<Edge, ItemEdge | ItemTaxiEdge> }) {
 	if (
 		item === 'Command Center Warp Capsule' ||
 		item === 'Ludibrium Warp Capsule' ||
@@ -142,6 +142,15 @@ function ItemContent({ item }: { item: Item }) {
 	if (item === 'Return to New Leaf City Scroll') return <Text>Only works within Victoria Island</Text>;
 
 	if (item === 'Fruit Milk' || item === 'Strawberry Milk') return <Text>Only works within Zipangu</Text>;
+
+	if (item === 'Return Scroll - Nearest Town') {
+		if (from === 'Korean Folk Town' && to === 'Aqua Road')
+			return (
+				<Text>
+					Use in <ExternalLink href="https://maplelegends.com/lib/map?id=230030200">The Sharp Unknown</ExternalLink>
+				</Text>
+			);
+	}
 
 	return null;
 }
