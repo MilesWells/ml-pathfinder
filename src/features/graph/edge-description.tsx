8568460a974f505ec4ac84e1669c5cdb5671c66a
Miles Wells <mileswells93@gmail.com>
2025-06-27 01:34:41 -0400

    better tooltips

--- src/features/graph/edge-description.tsx
+++ src/features/graph/edge-description.tsx
@@ -3,12 +3,13 @@
 import { Box, Center, Group, Popover, PopoverDropdown, PopoverTarget, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { ExternalLink } from '@/ui/external-link';
+import { type Item, itemDetailsMap } from '../items';
 import { ItemIcon } from '../items/item-icon';
 import { MesosIcon } from '../items/mesos-icon';
 import { npcDetailsMap } from '../npcs';
 import { TaxiIcon } from '../pathfinder/taxi-icon';
 import { WalkIcon } from '../pathfinder/walk-icon';
-import type { Edge, ItemTaxiEdge, SpinelEdge, TaxiEdge, TimedTaxiEdge } from './edges';
+import type { Edge, ItemEdge, ItemTaxiEdge, SpinelEdge, TaxiEdge, TimedTaxiEdge } from './edges';
 
 export type EdgeDescription = {
 	edge: Edge;
@@ -17,13 +18,33 @@ export type EdgeDescription = {
 export function EdgeDescription({ edge }: EdgeDescription) {
 	const label = `${edge.from} -> ${edge.to}`;
 
+	let content = <Text>{label}</Text>;
+	if (edge.method === 'Item') content = <ItemEdgeCost edge={edge} label={label} />;
+	else if (edge.method !== 'Walk') content = <NpcEdge edge={edge} label={label} />;
+
 	return (
 		<Group align="center">
 			<Icon edge={edge} />
 
-			{'npc' in edge ? <NpcEdge edge={edge} label={label} /> : <Text>{label}</Text>}
+			{content}
 		</Group>
 	);
+}
+
+function Icon({ edge }: { edge: Edge }) {
+	switch (edge.method) {
+		case 'Walk':
+			return <WalkIcon />;
+		case 'Taxi':
+		case 'Timed Taxi':
+		case 'Spinel':
+			if (edge.mesos === 0) return <TaxiIcon />;
+
+			return <MesosIcon mesos={edge.mesos} />;
+		case 'Item':
+		case 'Item Taxi':
+			return <ItemIcon item={edge.item} />;
+	}
 }
 
 type NPCEdge = Extract<Edge, TaxiEdge | TimedTaxiEdge | SpinelEdge | ItemTaxiEdge>;
@@ -55,6 +76,8 @@ function NpcEdge({ edge, label }: { edge: NPCEdge; label: string }) {
 					</Box>
 
 					<EdgeCost edge={edge} />
+
+					{'item' in edge ? <ItemConsumption item={edge.item} /> : null}
 				</Center>
 			</PopoverDropdown>
 		</Popover>
@@ -82,18 +105,49 @@ function EdgeCost({ edge }: { edge: NPCEdge }) {
 	);
 }
 
-function Icon({ edge }: { edge: Edge }) {
-	switch (edge.method) {
-		case 'Walk':
-			return <WalkIcon />;
-		case 'Taxi':
-		case 'Timed Taxi':
-		case 'Spinel':
-			if (edge.mesos === 0) return <TaxiIcon />;
+function ItemEdgeCost({ edge, label }: { edge: Extract<Edge, ItemEdge | ItemTaxiEdge>; label: string }) {
+	const [opened, { close, open }] = useDisclosure(false);
 
-			return <MesosIcon mesos={edge.mesos} />;
-		case 'Item':
-		case 'Item Taxi':
-			return <ItemIcon item={edge.item} />;
-	}
+	return (
+		<Popover opened={opened} position="right" shadow="md" withArrow>
+			<PopoverTarget>
+				<Center onMouseEnter={open} onMouseLeave={close}>
+					<ExternalLink href={itemDetailsMap[edge.item].docsLink}>{label}</ExternalLink>
+				</Center>
+			</PopoverTarget>
+
+			<PopoverDropdown style={{ pointerEvents: 'none' }}>
+				<Center style={{ flexDirection: 'column' }}>
+					<Text c="maplelegends-blue.6" component="span" fw="bold">
+						{edge.item}
+					</Text>
+
+					<ItemContent item={edge.item} />
+
+					<ItemConsumption item={edge.item} />
+				</Center>
+			</PopoverDropdown>
+		</Popover>
+	);
+}
+
+function ItemContent({ item }: { item: Item }) {
+	if (
+		item === 'Command Center Warp Capsule' ||
+		item === 'Ludibrium Warp Capsule' ||
+		item === 'Omega Sector Warp Capsule'
+	)
+		return <Text>Only works within Ludus Lake</Text>;
+
+	if (item === 'Return to New Leaf City Scroll') return <Text>Only works within Victoria Island</Text>;
+
+	if (item === 'Fruit Milk' || item === 'Strawberry Milk') return <Text>Only works within Zipangu</Text>;
+
+	return null;
+}
+
+function ItemConsumption({ item }: { item: Item }) {
+	if (item === 'VIP Ticket to Florina Beach' || item === 'Gate Pass') return <Text>Not consumed on use</Text>;
+
+	return <Text c="kimmy-red.5">Consumed on use</Text>;
 }
--- src/features/items/item-icon.tsx
+++ src/features/items/item-icon.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Center } from '@mantine/core';
+import { ExternalLink } from '@/ui/external-link';
 import { type Item, itemDetailsMap } from '.';
 
 export type ItemIconProps = {
@@ -11,16 +12,19 @@ export function ItemIcon({ item }: ItemIconProps) {
 	const image = itemDetailsMap[item].image;
 
 	return (
-		<Center
-			styles={{
-				root: {
-					height: 30,
-					width: 30,
-				},
-			}}
-			title={item}
-		>
-			<img alt={item} src={image} />
-		</Center>
+		<ExternalLink href={itemDetailsMap[item].docsLink}>
+			<Center
+				styles={{
+					root: {
+						cursor: 'pointer',
+						height: 30,
+						width: 30,
+					},
+				}}
+				title={item}
+			>
+				<img alt={item} src={image} />
+			</Center>
+		</ExternalLink>
 	);
 }
